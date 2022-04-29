930ddb00be928d0c4b82b43ec3daab4396590297
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2022-04-29 23:13:49 +0700

    update schedulehtml-add container-fluid

--- views/themes/perfex/views/schedules/schedulehtml.php
+++ views/themes/perfex/views/schedules/schedulehtml.php
@@ -13,8 +13,8 @@
    <div class="top" data-sticky data-sticky-class="preview-sticky-header">
       <div class="container preview-sticky-container">
          <div class="row">
-            <div class="col-md-12">
-
+            <div class="container-fluid">
+               <div class="col-md-12">
                <div class="col-md-2">
                   <h3 class="bold no-mtop schedule-html-number no-mbot">
                      <span class="sticky-visible hide">
@@ -71,10 +71,10 @@
                      <?php echo _l('client_go_to_dashboard'); ?>
                      </a>
                   <?php } ?>
+                  </div>
                </div>
-
-            </div>
                <div class="clearfix"></div>
+            </div>
          </div>
       </div>
    </div>
@@ -110,7 +110,7 @@
                   <?php if(!empty($schedule_members)){ ?>
                      <strong><?= _l('schedule_members_name') ?></strong>
                      <ul class="schedule_members">
-                     <?php
+                     <?php 
                         foreach($schedule_members as $member){
                           echo ('<li style="list-style:auto" class="member">' . $member['firstname'] .' '. $member['lastname'] .'</li>');
                          }
@@ -154,7 +154,7 @@
                </p>
                <?php } ?>
             </div>
-         </div>
+         </div>   
          <div class="row">
             <div class="col-md-12">
                <div class="table-responsive">
@@ -166,7 +166,7 @@
             </div>
 
             <?php if($schedule->assigned != 0 && get_option('show_assigned_on_schedules') == 1){ ?>
-
+            
             <div class="col-md-12">
                <div class="col-md-12 schedule-html-sale-agent">
                   <div class="bold"><?php echo _l('assigned_string'); ?>:</div>
