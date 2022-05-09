c1d7d7689a6368611cfbeeae42fd45e3b5130126
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2022-05-10 06:45:40 +0700

    v1.0.3

--- install.php
+++ install.php
@@ -14,17 +14,17 @@ if (!$CI->db->table_exists(db_prefix() . 'schedules')) {
       `number` int(11) NOT NULL DEFAULT 0,
       `prefix` varchar(50) DEFAULT NULL,
       `number_format` int(11) NOT NULL DEFAULT 0,
+      `formatted_number` varchar(20) DEFAULT NULL,
       `hash` varchar(32) DEFAULT NULL,
       `datecreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      `date` date NOT NULL DEFAULT NULL,
+      `date` date DEFAULT NULL,
       `expirydate` date DEFAULT NULL,
-      `inspector_name` date DEFAULT NULL,
       `addedfrom` int(11) NOT NULL DEFAULT 0,
       `status` int(11) NOT NULL DEFAULT 1,
       `clientnote` text DEFAULT NULL,
       `adminnote` text DEFAULT NULL,
-      `invoiceid` int(11) DEFAULT NULL,
-      `invoiced_date` datetime DEFAULT NULL,
+      `jobreportid` int(11) DEFAULT NULL,
+      `jobreport_date` datetime DEFAULT NULL,
       `terms` text DEFAULT NULL,
       `reference_no` varchar(100) DEFAULT NULL,
       `assigned` int(11) NOT NULL DEFAULT 0,
@@ -50,11 +50,13 @@ if (!$CI->db->table_exists(db_prefix() . 'schedules')) {
       `acceptance_date` datetime DEFAULT NULL,
       `acceptance_ip` varchar(40) DEFAULT NULL,
       `signature` varchar(40) DEFAULT NULL,
-      `short_link` varchar(100) DEFAULT NULL
+      `short_link` varchar(100) DEFAULT NULL,
+      `inspector_name` varchar(100) DEFAULT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=" . $CI->db->char_set . ';');
 
     $CI->db->query('ALTER TABLE `' . db_prefix() . 'schedules`
       ADD PRIMARY KEY (`id`),
+      ADD UNIQUE( `number`),
       ADD KEY `signed` (`signed`),
       ADD KEY `status` (`status`),
       ADD KEY `clientid` (`clientid`),
@@ -122,23 +124,56 @@ if (!$CI->db->table_exists(db_prefix() . 'schedule_items')) {
       MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1');
 }
 
+$CI->db->query("
+INSERT INTO `tblemailtemplates` (`type`, `slug`, `language`, `name`, `subject`, `message`, `fromname`, `fromemail`, `plaintext`, `active`, `order`) VALUES
+('schedule', 'schedule-send-to-client', 'english', 'Send schedule to Customer', 'schedule # {schedule_number} created', '<span style=\"font-size: 12pt;\">Dear {contact_firstname} {contact_lastname}</span><br /><br /><span style=\"font-size: 12pt;\">Please find the attached schedule <strong># {schedule_number}</strong></span><br /><br /><span style=\"font-size: 12pt;\"><strong>schedule status:</strong> {schedule_status}</span><br /><br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /><br /><span style=\"font-size: 12pt;\">We look forward to your communication.</span><br /><br /><span style=\"font-size: 12pt;\">Kind Regards,</span><br /><span style=\"font-size: 12pt;\">{email_signature}<br /></span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-already-send', 'english', 'schedule Already Sent to Customer', 'schedule # {schedule_number} ', '<span style=\"font-size: 12pt;\">Dear {contact_firstname} {contact_lastname}</span><br /> <br /><span style=\"font-size: 12pt;\">Thank you for your schedule request.</span><br /> <br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /> <br /><span style=\"font-size: 12pt;\">Please contact us for more information.</span><br /> <br /><span style=\"font-size: 12pt;\">Kind Regards,</span><br /><span style=\"font-size: 12pt;\">{email_signature}</span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-declined-to-staff', 'english', 'schedule Declined (Sent to Staff)', 'Customer Declined schedule', '<span style=\"font-size: 12pt;\">Hi</span><br /> <br /><span style=\"font-size: 12pt;\">Customer ({client_company}) declined schedule with number <strong># {schedule_number}</strong></span><br /> <br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /> <br /><span style=\"font-size: 12pt;\">{email_signature}</span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-accepted-to-staff', 'english', 'schedule Accepted (Sent to Staff)', 'Customer Accepted schedule', '<span style=\"font-size: 12pt;\">Hi</span><br /> <br /><span style=\"font-size: 12pt;\">Customer ({client_company}) accepted schedule with number <strong># {schedule_number}</strong></span><br /> <br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /> <br /><span style=\"font-size: 12pt;\">{email_signature}</span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-thank-you-to-customer', 'english', 'Thank You Email (Sent to Customer After Accept)', 'Thank for you accepting schedule', '<span style=\"font-size: 12pt;\">Dear {contact_firstname} {contact_lastname}</span><br /> <br /><span style=\"font-size: 12pt;\">Thank for for accepting the schedule.</span><br /> <br /><span style=\"font-size: 12pt;\">We look forward to doing business with you.</span><br /> <br /><span style=\"font-size: 12pt;\">We will contact you as soon as possible.</span><br /> <br /><span style=\"font-size: 12pt;\">Kind Regards,</span><br /><span style=\"font-size: 12pt;\">{email_signature}</span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-expiry-reminder', 'english', 'schedule Expiration Reminder', 'schedule Expiration Reminder', '<p><span style=\"font-size: 12pt;\">Hello {contact_firstname} {contact_lastname}</span><br /><br /><span style=\"font-size: 12pt;\">The schedule with <strong># {schedule_number}</strong> will expire on <strong>{schedule_expirydate}</strong></span><br /><br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /><br /><span style=\"font-size: 12pt;\">Kind Regards,</span><br /><span style=\"font-size: 12pt;\">{email_signature}</span></p>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-send-to-client', 'english', 'Send schedule to Customer', 'schedule # {schedule_number} created', '<span style=\"font-size: 12pt;\">Dear {contact_firstname} {contact_lastname}</span><br /><br /><span style=\"font-size: 12pt;\">Please find the attached schedule <strong># {schedule_number}</strong></span><br /><br /><span style=\"font-size: 12pt;\"><strong>schedule status:</strong> {schedule_status}</span><br /><br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /><br /><span style=\"font-size: 12pt;\">We look forward to your communication.</span><br /><br /><span style=\"font-size: 12pt;\">Kind Regards,</span><br /><span style=\"font-size: 12pt;\">{email_signature}<br /></span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-already-send', 'english', 'schedule Already Sent to Customer', 'schedule # {schedule_number} ', '<span style=\"font-size: 12pt;\">Dear {contact_firstname} {contact_lastname}</span><br /> <br /><span style=\"font-size: 12pt;\">Thank you for your schedule request.</span><br /> <br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /> <br /><span style=\"font-size: 12pt;\">Please contact us for more information.</span><br /> <br /><span style=\"font-size: 12pt;\">Kind Regards,</span><br /><span style=\"font-size: 12pt;\">{email_signature}</span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-declined-to-staff', 'english', 'schedule Declined (Sent to Staff)', 'Customer Declined schedule', '<span style=\"font-size: 12pt;\">Hi</span><br /> <br /><span style=\"font-size: 12pt;\">Customer ({client_company}) declined schedule with number <strong># {schedule_number}</strong></span><br /> <br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /> <br /><span style=\"font-size: 12pt;\">{email_signature}</span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-accepted-to-staff', 'english', 'schedule Accepted (Sent to Staff)', 'Customer Accepted schedule', '<span style=\"font-size: 12pt;\">Hi</span><br /> <br /><span style=\"font-size: 12pt;\">Customer ({client_company}) accepted schedule with number <strong># {schedule_number}</strong></span><br /> <br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /> <br /><span style=\"font-size: 12pt;\">{email_signature}</span>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'staff-added-as-project-member', 'english', 'Staff Added as Project Member', 'New project assigned to you', '<p>Hi <br /><br />New schedule has been assigned to you.<br /><br />You can view the schedule on the following link <a href=\"{schedule_link}\">schedule__number</a><br /><br />{email_signature}</p>', '{companyname} | CRM', '', 0, 1, 0),
+('schedule', 'schedule-accepted-to-staff', 'english', 'schedule Accepted (Sent to Staff)', 'Customer Accepted schedule', '<span style=\"font-size: 12pt;\">Hi</span><br /> <br /><span style=\"font-size: 12pt;\">Customer ({client_company}) accepted schedule with number <strong># {schedule_number}</strong></span><br /> <br /><span style=\"font-size: 12pt;\">You can view the schedule on the following link: <a href=\"{schedule_link}\">{schedule_number}</a></span><br /> <br /><span style=\"font-size: 12pt;\">{email_signature}</span>', '{companyname} | CRM', '', 0, 1, 0);
+");
+/*
+ *
+ */
 
 // Add options for schedules
 add_option('delete_only_on_last_schedule', 1);
 add_option('schedule_prefix', 'SCH-');
 add_option('next_schedule_number', 1);
+add_option('default_schedule_assigned', 9);
 add_option('schedule_number_decrement_on_delete', 0);
 add_option('schedule_number_format', 4);
 add_option('schedule_year', date('Y'));
-add_option('schedule_auto_convert_to_invoice_on_client_accept', 1);
 add_option('exclude_schedule_from_client_area_with_draft_status', 1);
-add_option('predefined_clientnote_schedule', '- Staf diatas untuk melakukan riksa uji pada peralatan tersebut.<br />
+add_option('predefined_clientnote_schedule', '- Staf diatas untuk melakukan riksa uji pada peralatan tersebut.
 - Staf diatas untuk membuat dokumentasi riksa uji sesuai kebutuhan.');
-add_option('predefined_terms_schedule', '- Pelaksanaan riksa uji harus mengikuti prosedur yang ditetapkan perusahaan pemilik alat.<br />
+add_option('predefined_terms_schedule', '- Pelaksanaan riksa uji harus mengikuti prosedur yang ditetapkan perusahaan pemilik alat.
 - Dilarang membuat dokumentasi tanpa seizin perusahaan pemilik alat.');
-add_option('schedule_due_after', 7);
+add_option('schedule_due_after', 1);
 add_option('allow_staff_view_schedules_assigned', 1);
-add_option('view_schedule_only_logged_in', 1);
+add_option('show_assigned_on_schedules', 1);
+add_option('require_client_logged_in_to_view_schedule', 0);
+
 add_option('show_project_on_schedule', 1);
 add_option('schedules_pipeline_limit', 1);
 add_option('default_schedules_pipeline_sort', 1);
+add_option('schedule_accept_identity_confirmation', 1);
+
+
+/*
+
+DROP TABLE `tblschedules`;
+DROP TABLE `tblschedule_activity`, `tblschedule_items`, `tblschedule_members`;
+delete FROM `tbloptions` WHERE `name` LIKE '%schedule%';
+DELETE FROM `tblemailtemplates` WHERE `type` LIKE 'schedule';
+
+
+
+*/
--- schedules.php
+++ schedules.php
@@ -3,9 +3,9 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
-Module Name: Schedules
+Module Name: schedules
 Description: Default module for defining schedules
-Version: 1.0.2
+Version: 1.0.1
 Requires at least: 2.3.*
 */
 
@@ -21,6 +21,8 @@ hooks()->add_action('admin_init', 'schedules_permissions');
 hooks()->add_action('clients_init', 'schedules_clients_area_menu_items');
 
 hooks()->add_action('staff_member_deleted', 'schedules_staff_member_deleted');
+
+hooks()->add_action('after_schedule_updated', 'schedule_create_assigned_qrcode');
 
 hooks()->add_filter('migration_tables_to_replace_old_links', 'schedules_migration_tables_to_replace_old_links');
 hooks()->add_filter('global_search_result_query', 'schedules_global_search_result_query', 10, 3);
@@ -52,7 +54,7 @@ function schedules_staff_member_deleted($data)
 function schedules_global_search_result_output($output, $data)
 {
     if ($data['type'] == 'schedules') {
-        $output = '<a href="' . admin_url('schedules/schedule/' . $data['result']['id']) . '">' . format_schedule_number($data['result']['number']) . '</a>';
+        $output = '<a href="' . admin_url('schedules/schedule/' . $data['result']['id']) . '">' . format_schedule_number($data['result']['id']) . '</a>';
     }
 
     return $output;
@@ -62,11 +64,26 @@ function schedules_global_search_result_query($result, $q, $limit)
 {
     $CI = &get_instance();
     if (has_permission('schedules', '', 'view')) {
-        // Schedules
-        $CI->db->select()->from(db_prefix() . 'schedules')->like(db_prefix() . 'projects.name', $q)->or_like(db_prefix() . 'clients.company', $q)->limit($limit);
-        $CI->db->join(db_prefix() . 'projects',db_prefix() . 'schedules.project_id='.db_prefix() .'projects.id', 'left');
+
+        // schedules
+        $CI->db->select()
+           ->from(db_prefix() . 'schedules')
+           ->like(db_prefix() . 'schedules.formatted_number', $q)->limit($limit);
+        
+        $result[] = [
+                'result'         => $CI->db->get()->result_array(),
+                'type'           => 'schedules',
+                'search_heading' => _l('schedules'),
+            ];
+        
+        if(isset($result[0]['result'][0]['id'])){
+            return $result;
+        }
+
+        // schedules
+        $CI->db->select()->from(db_prefix() . 'schedules')->like(db_prefix() . 'clients.company', $q)->or_like(db_prefix() . 'schedules.formatted_number', $q)->limit($limit);
         $CI->db->join(db_prefix() . 'clients',db_prefix() . 'schedules.clientid='.db_prefix() .'clients.userid', 'left');
-        $CI->db->order_by(db_prefix() . 'projects.name', 'ASC');
+        $CI->db->order_by(db_prefix() . 'clients.company', 'ASC');
 
         $result[] = [
                 'result'         => $CI->db->get()->result_array(),
@@ -102,34 +119,6 @@ function schedules_permissions()
     register_staff_capabilities('schedules', $capabilities, _l('schedules'));
 }
 
-function schedules_notification()
-{
-    $CI = &get_instance();
-    $CI->load->model('schedules/schedules_model');
-    $schedules = $CI->schedules_model->get('', true);
-    foreach ($schedules as $schedule) {
-        $achievement = $CI->schedules_model->calculate_schedule_achievement($schedule['id']);
-
-        if ($achievement['percent'] >= 100) {
-            if (date('Y-m-d') >= $schedule['end_date']) {
-                if ($schedule['notify_when_achieve'] == 1) {
-                    $CI->schedules_model->notify_staff_members($schedule['id'], 'success', $achievement);
-                } else {
-                    $CI->schedules_model->mark_as_notified($schedule['id']);
-                }
-            }
-        } else {
-            // not yet achieved, check for end date
-            if (date('Y-m-d') > $schedule['end_date']) {
-                if ($schedule['notify_when_fail'] == 1) {
-                    $CI->schedules_model->notify_staff_members($schedule['id'], 'failed', $achievement);
-                } else {
-                    $CI->schedules_model->mark_as_notified($schedule['id']);
-                }
-            }
-        }
-    }
-}
 
 /**
 * Register activation module hook
@@ -141,6 +130,19 @@ function schedules_module_activation_hook()
     $CI = &get_instance();
     require_once(__DIR__ . '/install.php');
 }
+
+/**
+* Register deactivation module hook
+*/
+register_deactivation_hook(SCHEDULES_MODULE_NAME, 'schedules_module_deactivation_hook');
+
+function schedules_module_deactivation_hook()
+{
+
+     log_activity( 'Hello, world! . schedules_module_deactivation_hook ' );
+}
+
+//hooks()->add_action('deactivate_' . $module . '_module', $function);
 
 /**
 * Register language files, must be registered if the module is using languages
@@ -159,7 +161,7 @@ function schedules_module_init_menu_items()
             'name'       => _l('schedule'),
             'url'        => 'schedules',
             'permission' => 'schedules',
-            'position'   => 56,
+            'position'   => 57,
             ]);
 
     if (has_permission('schedules', '', 'view')) {
@@ -192,6 +194,26 @@ function schedules_clients_area_menu_items()
     }
 }
 
+/**
+ * [perfex_dark_theme_settings_tab net menu item in setup->settings]
+ * @return void
+ */
+function schedules_settings_tab()
+{
+    $CI = &get_instance();
+    $CI->app_tabs->add_settings_tab('schedules', [
+        'name'     => _l('settings_group_schedules'),
+        //'view'     => module_views_path(SCHEDULES_MODULE_NAME, 'admin/settings/includes/schedules'),
+        'view'     => 'schedules/schedules_settings',
+        'position' => 51,
+    ]);
+}
 
 $CI = &get_instance();
 $CI->load->helper(SCHEDULES_MODULE_NAME . '/schedules');
+
+if(($CI->uri->segment(0)=='admin' && $CI->uri->segment(1)=='schedules') || $CI->uri->segment(1)=='schedules'){
+    $CI->app_css->add(SCHEDULES_MODULE_NAME.'-css', base_url('modules/'.SCHEDULES_MODULE_NAME.'/assets/css/'.SCHEDULES_MODULE_NAME.'.css'));
+    $CI->app_scripts->add(SCHEDULES_MODULE_NAME.'-js', base_url('modules/'.SCHEDULES_MODULE_NAME.'/assets/js/'.SCHEDULES_MODULE_NAME.'.js'));
+}
+
