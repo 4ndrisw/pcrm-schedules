4d4605a06950910a3956572590f195acf60a7408
4ndr1sw <4ndr1sw@gmail.com>
2022-05-13 15:47:56 +0700

    change max-width
    change max-width to more space

--- views/themes/perfex/views/schedules/schedulehtml.php
+++ views/themes/perfex/views/schedules/schedulehtml.php
@@ -13,7 +13,7 @@
    <div class="top" data-sticky data-sticky-class="preview-sticky-header">
       <div class="container preview-sticky-container">
          <div class="row">
-            <div class="col-md-10">
+            <div class="col-md-12">
                <div class="col-md-2">
                   <h3 class="bold no-mtop schedule-html-number no-mbot">
                      <span class="sticky-visible hide">
